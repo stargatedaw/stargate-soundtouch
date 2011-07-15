22cb20b6b1c37cef6334d8b86846b1008c0f3b72
oparviai <oparviai@f3a24b6a-cf45-0410-b55a-8c22e2698227>
2011-07-15 19:27:10 +0000

    Set version to 1.6.0

--- include/SoundTouch.h
+++ include/SoundTouch.h
@@ -79,10 +79,10 @@ namespace soundtouch
 {
 
 /// Soundtouch library version string
-#define SOUNDTOUCH_VERSION          "1.5.1pre"
+#define SOUNDTOUCH_VERSION          "1.6.0"
 
 /// SoundTouch library version id
-#define SOUNDTOUCH_VERSION_ID       (10509)
+#define SOUNDTOUCH_VERSION_ID       (10600)
 
 //
 // Available setting IDs for the 'setSetting' & 'get_setting' functions:
