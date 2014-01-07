2d91306ac03965da9981b28c5b7664f9d82e1142
oparviai <oparviai@f3a24b6a-cf45-0410-b55a-8c22e2698227>
2014-01-07 19:26:29 +0000

    Changed version string to 1.8.0

--- include/SoundTouch.h
+++ include/SoundTouch.h
@@ -79,10 +79,10 @@ namespace soundtouch
 {
 
 /// Soundtouch library version string
-#define SOUNDTOUCH_VERSION          "1.7.2 (dev)"
+#define SOUNDTOUCH_VERSION          "1.8.0"
 
 /// SoundTouch library version id
-#define SOUNDTOUCH_VERSION_ID       (10702)
+#define SOUNDTOUCH_VERSION_ID       (10800)
 
 //
 // Available setting IDs for the 'setSetting' & 'get_setting' functions:
