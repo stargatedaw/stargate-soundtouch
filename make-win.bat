099a6240eb2aaa3b70d2a5b9451aa4e71cebf949
oparviai <oparviai@f3a24b6a-cf45-0410-b55a-8c22e2698227>
2014-04-06 18:03:48 +0000

    Added x64 compilation to Visual Studio project files

--- make-win.bat
+++ make-win.bat
@@ -37,10 +37,12 @@ devenv source\SoundTouch\SoundTouch.vcproj /build release
 devenv source\SoundStretch\SoundStretch.vcproj /upgrade
 devenv source\SoundStretch\SoundStretch.vcproj /build debug
 devenv source\SoundStretch\SoundStretch.vcproj /build release
+devenv source\SoundStretch\SoundStretch.vcproj /build releasex64
 
 devenv source\SoundTouchDll\SoundTouchDll.vcproj /upgrade
 devenv source\SoundTouchDll\SoundTouchDll.vcproj /build debug
 devenv source\SoundTouchDll\SoundTouchDll.vcproj /build release
+devenv source\SoundTouchDll\SoundTouchDll.vcproj /build releasex64
 
 @goto end
 
