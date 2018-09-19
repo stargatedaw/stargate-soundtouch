bdbe1bf5513c1f9277d19a77cce3142ddbbac4b1
ggfan <gfan@google.com>
2018-09-19 04:08:51 -0700

    Adding gradle build for Android example

--- source/Android-lib/jni/Application.mk
+++ source/Android-lib/jni/Application.mk
@@ -4,6 +4,6 @@
 
 APP_ABI := all #armeabi-v7a armeabi
 APP_OPTIM := release
-APP_STL := stlport_static
+APP_STL := c++_static
 APP_CPPFLAGS := -fexceptions # -D SOUNDTOUCH_DISABLE_X86_OPTIMIZATIONS
 
