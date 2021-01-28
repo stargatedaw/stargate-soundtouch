f38cfa685047684920171c2add7dafa9dce7d191
Olli <oparviai 'at' iki.fi>
2021-01-28 20:19:06 +0200

    Call "clear()" after changing anti-alias filter on/off
    Call "clear()" after changing anti-alias filter on/off to prefill
    buffers appropriately.

--- source/SoundTouch/RateTransposer.cpp
+++ source/SoundTouch/RateTransposer.cpp
@@ -78,6 +78,7 @@ void RateTransposer::enableAAFilter(bool newMode)
 #ifndef SOUNDTOUCH_PREVENT_CLICK_AT_RATE_CROSSOVER
     // Disable Anti-alias filter if desirable to avoid click at rate change zero value crossover
     bUseAAFilter = newMode;
+    clear();
 #endif
 }
 
