774257ab5f6efbe1c6cfd008ba20a897d35fd3e4
Olli <oparviai'at'iki.fi>
2022-10-31 18:51:35 +0200

    Small updates to dynamic-link libary build script & pascal example
    Signed-off-by: Olli <oparviai'at'iki.fi>

--- source/SoundTouchDLL/make-gnu-dll.sh
+++ source/SoundTouchDLL/make-gnu-dll.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
-# This script compiles SoundTouch dynamic-link library for GNU environment 
+# This script is deprecated. Don't use this, the makefile can now compile
+# the dynamic-link library 'libSoundTouchDLL.so' automatically.
+#
+# This script compiles SoundTouch dynamic-link library for GNU environment
 # with wrapper functions that are easier to import to Java / Mono / etc
 #
 
@@ -11,11 +14,15 @@ if [[ $arch == *"86"* ]]; then
     # Intel x86/x64 architecture
     flags="$flags -mstackrealign -msse"
 
-    if [[ $arch == *"_64" ]]; then	
+    if [[ $arch == *"_64" ]]; then
         flags="$flags -fPIC"
     fi
 fi
 
+echo "*************************************************************************"
+echo "NOTE: Rather use the makefile that can now build the dynamic-link library"
+echo "*************************************************************************"
+echo ""
 echo "Building SoundTouchDLL for $arch with flags:$flags"
 
 g++ -O3 -ffast-math -shared $flags -DDLL_EXPORTS -fvisibility=hidden -I../../include \
